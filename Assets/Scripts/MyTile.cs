450477e8566d829903bf2801595e5dafc4ff6998
blueshinymarble <blueshinymarble@gmail.com>
2018-07-06 12:43:05 +0900

    some minor changes

---
+++ Assets/Scripts/MyTile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTile : MonoBehaviour
+{
+    public GameObject[] TerrainTiles;
+    public GameObject powerPlant;
+
+    // Use this for initialization
+    void Start()
+    {
+        Board board = GameObject.Find("Board").GetComponent<Board>();
+        while (gameObject.transform.childCount < 1)
+        {
+            board.ChooseTile((Board.TerrainTilesEnum)Random.Range(0, 6), transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        //play selection animation;
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameObject.tag != "Hazard")
+        {
+            GameObject newBase = Instantiate(powerPlant, transform.position, Quaternion.identity);
+            newBase.transform.parent = gameObject.transform;
+            MinimiseTerrain();
+        }
+    }
+
+    void MinimiseTerrain()
+    {
+        Animator[] myTerrainTileAnimators = gameObject.GetComponentsInChildren<Animator>();
+        foreach (Animator anim in myTerrainTileAnimators)
+        {
+            anim.Play("terrain minimise");
+        }
+    }
+
+}
--- Assets/Scripts/TurnFlowManager.cs
+++ Assets/Scripts/TurnFlowManager.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TurnFlowManager : MonoBehaviour {
+public class TurnFlowManager : MonoBehaviour
+{
 
-    public enum State { }
+    public enum State {firstRound, roundBegins, bloom, action, scoring}
     public State currentState;
+
+    private GameObject announcer;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        announcer = GameObject.Find("Announcer");
+        currentState = State.firstRound;
+        SpaceForBase();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    public void SpaceForBase()
+    {
+        announcer.GetComponentInChildren<Text>().text = "Choose a space to place your base.";
+    }
 }
