a1bef0e063cfe0efb4816883777a95379ae5ff58
blueshinymarble <blueshinymarble@gmail.com>
2018-07-31 21:54:52 +0900

    Collisions work but the position of the colliders is such that sometimes they dont collide so making the colliders bigger should be the solution

--- Assets/Scripts/DamagePip.cs
+++ Assets/Scripts/DamagePip.cs
@@ -21,4 +21,12 @@ public class DamagePip : MonoBehaviour
     {
 		
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.parent.tag == "Legal")
+        {
+            Debug.Log("Collision detected");
+        }
+    }
 }
